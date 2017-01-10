001f4c549d8f1d09a2c836e1ca85d36082d055b6
Peter Lu <peterlu6@stanford.edu>
2017-01-10 14:18:34 -0800

    date substitution

--- preprocessor_edition2.py
+++ preprocessor_edition2.py
@@ -13,18 +13,19 @@ FEATURES:
 5. substitute all one digit numbers with strings
 6. remove extraneous whitespace
 7. remove new lines
+8. mark dates by time before input date
 
 TODO:
-1. recognize dates
-2. mark dates by time before input date
-3. put events in chronological order
-4. possibly merge words that are separated by dashes?
+1. put events in chronological order
+2. integrate ontology
 
 
-LAST UPDATED: 1/8/2017
+LAST UPDATED: 1/10/2017
 '''
 import re #regexes
 import sys #command line arguments
+
+date_input = "" #global for date input (can be removed if we use lambdas later)
 
 def concatenate_into_string(infile):
 	total_text = ""
@@ -51,12 +52,42 @@ def remove_forbidden_tokens(output, exclude_list):
 		output = re.sub(r""+re.escape(item)+r"", "", output)
 	return output
 
+def time_elapsed(match_group):
+	date_input_group = re.match(r"(\d+)\/(\d+)\/(\d+)", date_input)
+	input_month = int(date_input_group.group(1))
+	input_day = int(date_input_group.group(2))
+	input_year = int(date_input_group.group(3))
+	matched_month = int(match_group.group(1))
+	matched_day = int(match_group.group(2))
+	matched_year = int(match_group.group(3))
+
+	if input_year - matched_year > 2:
+		return "twoyearsago"
+	elif input_year - matched_year > 1:
+		return "oneyearago"
+	elif input_month - matched_month > 9:
+		return "ninemonthsago"
+	elif input_month - matched_month > 6:
+		return "sixmonthsago"
+	elif input_month - matched_month > 3:
+		return "threemonthsago"
+	elif input_month - matched_month > 2:
+		return "twomonthsago"
+	elif input_month - matched_month > 1:
+		return "onemonthago"
+	elif input_day - matched_day > 14:
+		return "twoweeksago"
+	elif input_day - matched_day > 7:
+		return "oneweekago"
+	else:
+		return "recent"
 
 '''
 The primary method. Takes input as a string and an exclusion list of strings and outputs a string.
 '''
 def preprocess(inputstr, exclude_list):
-	output = inputstr.lower()
+	output = inputstr.lower() #tolowercase
+	output = re.sub(r"(\d+)\/(\d+)\/(\d+)", time_elapsed, output) #processes dates of the form MM/DD/YYYY
 	output = re.sub(r"\d\d+", "", output) #remove multidigit numbers
 	output = re.sub(r'[;()"%\'\.\/\:\?\-,]', '', output) #remove special characters
 	output = substitute_word_for_digit(output) #substitute single digit numbers with words
@@ -71,7 +102,8 @@ def main():
 	num_arguments = len(sys.argv)
 	infilename = sys.argv[1]
 	outfilename = sys.argv[2]
-	date = sys.argv[3]
+	global date_input
+	date_input = sys.argv[3]
 
 	exclude_list = []
 	for i in range(4, num_arguments): #populate exclude list
